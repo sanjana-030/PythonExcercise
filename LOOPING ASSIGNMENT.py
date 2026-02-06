807e3936f49235848f8bea7087b2206d51fb8adf
Karan <mamtarajkumar1212@gmail.com>
2026-02-06 15:09:06 -0800

    UPDATED

---
+++ LOOPING ASSIGNMENT.py
@@ -0,0 +1,181 @@
+#FOR Loop – Programming Questions
+
+#1. Write a program to print numbers from 1 to 100.
+"""
+for i in range(1,101,1):
+    print(i)
+    
+#2. Write a program to print all even numbers between 1 and 50.
+for i in range(1,51,1):
+    if i%2==0:
+        print(i)
+   
+#3. Write a program to print the sum of first n natural numbers.
+n = int(input("enter a number:"))
+sum =0
+for i in range(1,n+1):
+        sum=sum+i
+        print(i)
+print("sum of number:",sum)  
+
+#4. Write a program to print the multiplication table of a given number.        
+num = int(input("enter a number:"))
+for i in range(1,11):
+    print(num*i)
+    
+#5. Write a program to print all elements of a list using a for loop.
+listt =[23,45,78,15,90,67]
+for i in listt:
+    print(i)
+    
+#6. Write a program to count the number of vowels in a string.
+vowel ="aeiou"
+count =0
+for i in vowel:
+    count=count+1
+    print(i)
+print("count of vowels",count)
+ 
+#7. Write a program to find the largest number in a list.
+#8. Write a program to print all prime numbers between 1 and 100.
+for i in range(1,101):
+    if i%2==0:
+        print(i)
+        
+#9. Write a program to calculate the factorial of a number using a for loop.
+n= int(input("enter a number"))
+fl = 1
+for i in range(1,n+1):
+    fl=fl*i
+    print(i)
+print("factorial of number",fl)
+
+#10. Write a program to print the reverse of a string using a for loop.
+el ="govtschool"
+reverse =""
+for i in el:
+    reverse = i+reverse
+print(reverse)
+    
+#WHILE Loop – Programming Questions
+#11. Write a program to print numbers from 1 to 50 using a while loop
+a=1
+while a<=50:
+    print(a)
+    a=a+1
+   
+#12. Write a program to print all odd numbers between 1 and 50.
+a=1
+while a<=50:
+    if a%2==1:
+        print(a)
+    a=a+1
+ 
+
+
+#13. Write a program to calculate the sum of digits of a number.
+num =int(input("enter a number:"))
+a =1
+add = 0
+while a<=num:
+    rem = num%10
+    add = add+rem
+    num = num//10
+print("sum of digits:",add)
+#14. Write a program to reverse a number using a while loop
+num= int(input("enter a number:"))
+a = 1
+add = 0
+while a<=num:
+    rem = num%10
+    add = add*10+rem
+    num = num//10
+print("reverse number:",add)
+
+#15. Write a program to find the factorial of a number using a while loop
+num =int(input("enter a number:"))
+a = 1
+fl = 1
+while a<=num:
+    fl=fl*a
+    a=a+1
+print(fl)
+
+#16. Write a program to keep taking input from the user until the user enters 0.
+num =int(input("enter a number:"))
+a = 0
+while num!=a:
+     num=int(input("enter another number:"))
+     if num==a:
+         print("number found")
+         
+#17. Write a program to find the largest digit in a number.
+num = int(input("enter a number:"))
+maxx = 0
+while num>0:
+    rem = num%10
+    if maxx<rem:
+        maxx=rem
+    num = num//10
+print("largest number",maxx)
+
+#18. Write a program to check whether a number is a palindrome.
+num= int(input("enter a number:"))
+a = 1
+add = 0
+copy = num
+while a<=num:
+    rem = num%10
+    add = add*10+rem
+    num = num//10
+print("reverse number:",add)
+if copy==add:
+    print("palindrome")
+else:
+    print("not palindrome")
+
+
+#19. Write a program to print the Fibonacci series up to n terms.
+#0, 1, 1, 2, 3, 5, 8, 13
+a = 0
+b = 1
+print(0)
+while a<=10:
+    c = a+b
+    a = b
+    b = c
+    print(a)
+    
+#20. Write a program to implement a number guessing game using a while loop.
+num = int(input("enter a number:"))
+a = 10
+while num!=a:
+    num =int(input("enter another number:"))
+    if num==a:
+        print("winner")
+        
+#21. Write a program to print a number pattern using loops.
+st =""
+for i in range(1,6):
+    st = st+str(i)
+    print(st)
+
+#22. Write a program to count the frequency of each character in a string..
+#23. Write a program to print all Armstrong numbers between 1 and 1000.
+#24. Write a program to simulate an ATM menu using a while loop.
+
+#25. Write a program to find the GCD of two numbers using loops.
+n1 = int(input("enter a number:"))
+n2 = int(input("enter a number:"))
+gcd = 1              
+for i in range(1,n1+1):
+            if n1%i==0 and n2%i==0:
+                gcd = i
+print("GCD:",gcd)
+ """              
+               
+              
+
+
+
+
